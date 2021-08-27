41b98e88c6f14a3dc4e3ccc5d0431f81d62c22e3
Vet <vd188@yandex.ru>
2021-08-27 15:15:05 +0300

    render controller-info and add proto_test.ipynb

--- coursera_house/core/views.py
+++ coursera_house/core/views.py
@@ -1,8 +1,10 @@
 from django.urls import reverse_lazy
 from django.views.generic import FormView
+from django.conf import settings as conf_settings
 
 from .models import Setting
 from .form import ControllerForm
+import requests, json
 
 
 class ControllerView(FormView):
@@ -12,7 +14,13 @@ class ControllerView(FormView):
 
     def get_context_data(self, **kwargs):
         context = super(ControllerView, self).get_context_data()
-        context['data'] = {}
+        headers = {'Authorization': 'Bearer {}'.format(conf_settings.SMART_HOME_ACCESS_TOKEN)}  
+        r = requests.get(conf_settings.SMART_HOME_API_URL, headers=headers)
+        all_states = json.loads(r.text)
+        #context['data'] = {item["name"]: {'value': item["value"],'created': item['created'], \
+        #'updated': item['updated']} for item in all_states['data']}
+        context['data'] = {item["name"]: item["value"] for item in all_states['data']}
+
         return context
 
     def get_initial(self):
